cf97e857532d2af06412f92b52aca160903c34d2
João Lucas Brasileiro <lucas_jl07@hotmail.com>
2023-11-08 16:36:59 -0300

    Funçoes da academia python

---
+++ funcoes_academia.py
@@ -0,0 +1,43 @@
+import random
+
+def filtra(palavras,letras):
+    palavras_novas = set()
+
+    for palavra in palavras:
+        palavra = palavra.strip()
+        palavra = palavra.lower()
+        palavra_normais = ''.join(i for i in palavra if i.isalnum())
+
+        if len(palavra_normais) == letras:
+            palavras_novas.add(palavra_normais)
+    return list(palavras_novas)
+    
+
+
+def inicializa(palavras):
+    configuração = {
+        'n': len(palavras[0]),
+        'tentativas':len(palavras[0])+1,
+        'especuladas': [],
+        'sorteada': random.choice(palavras)
+        
+    }
+    return configuração
+
+def inidica_posicao(sorteada,especulada):
+    if len(especulada) != len(sorteada):
+        return []
+    
+    lista = []
+    for i in range(len(sorteada)):
+        if especulada[i] == sorteada[i]:
+            lista.append(0)
+        elif especulada[i] in sorteada:
+            lista.append(1)
+        elif especulada[i] not in sorteada:
+            lista.append(2)
+    
+    return lista
+        
+        
+        
